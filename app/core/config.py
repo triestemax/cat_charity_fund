62785e57e126593d4127f55d92e194d8d47cc848
Maxim <shabanov_maxim@mail.ru>
2024-03-20 10:56:53 +0300

    56

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str
+    app_title: str = 'Платформа благотворительности'
     app_description: str = 'Сервис для поддержки котиков!'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'Secret'
