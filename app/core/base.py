b3d362626412491e906d1c42d751c5c6d0d4bc95
Maxim <shabanov_maxim@mail.ru>
2024-03-20 10:10:38 +0300

    +3

--- app/core/base.py
+++ app/core/base.py
@@ -1,3 +1,3 @@
-"""Импорты класса Base и всех моделей для Alembic."""
-from app.core.db import Base # noqa
-from app.models import CharityProject, Donation, User # noqa 
+# Flake8: noqa F401
+from app.core.db import Base
+from app.models import CharityProject, Donation, User
